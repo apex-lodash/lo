465bad4172db01786b232128944b635dd6fcd4a7
Patrick Connelly <patrick@deadlypenguin.com>
2015-04-22 09:18:49 -0400

    Setting up travis ci

---
+++ build/scripts/deploy.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+set -ev
+
+SOL_ROOT="$TRAVIS_BUILD_DIR/build/solenopsis/scripts"
+
+cd $SOL_ROOT
+./bsolenopsis push
+./bsolenopsis run-tests
---
+++ build/scripts/setup_credentials.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+SOL_PROPFILE="$HOME/solenopsis.properties"
+SOL_HOMEDIR="$HOME/.solenopsis"
+SOL_CREDDIR="$SOL_HOMEDIR/credentials"
+SOL_CREDFILE="$SOL_CREDDIR/lodash.properties"
+
+SRC_DIR="$TRAVIS_BUILD_DIR/src"
+
+cat >$SOL_PROPFILE <<EOF
+solenopsis.ENVIRONMENTS=local lodash
+solenopsis.env.HOME=$SOL_HOMEDIR
+solenopsis.env.MASTER=local
+solenopsis.env.DEPENDENT=lodash
+solenopsis.env.local.HOME=$SRC_DIR
+
+sf.antFile=$TRAVIS_BUILD_DIR/build/ant/ant-salesforce-33.jar
+#sf.ignoreFile=$SRC_DIR/.sfdcignore
+EOF
+
+mkdir -p $SOL_CREDDIR
+
+cat >$SOL_CREDFILE <<EOF
+username = $SFDC_USERNAME
+password = $SFDC_PASSWORD
+token = $SFDC_TOKEN
+url = $SFDC_URL
+EOF
